dde763bbd8985fc43fa89ac1de7f11ecd64e4767
Ryan Baumann <ryan.baumann@gmail.com>
2017-07-05 17:13:21 -0400

    Build pn-dispatch WAR

--- navigator/run-mapping.sh
+++ navigator/run-mapping.sh
@@ -6,3 +6,4 @@ fi
 
 sed -i -e 's/localhost:8090/fuseki:8090/' /srv/data/papyri.info/git/navigator/pn-mapping/src/info/papyri/map.clj
 cd /srv/data/papyri.info/git/navigator/pn-mapping && lein run map-all
+cd /srv/data/papyri.info/git/navigator/pn-dispatcher && mvn clean package
