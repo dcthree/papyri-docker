540c61af12e54b9763fb36d4e7a90a4f4836e42f
Ryan Baumann <ryan.baumann@gmail.com>
2017-07-06 15:41:56 -0400

    Hook up solr indexing

--- navigator/run-mapping.sh
+++ navigator/run-mapping.sh
@@ -4,6 +4,10 @@ if [ ! -d "/srv/data/papyri.info/git/navigator" ]; then
   mkdir -p /srv/data/papyri.info/git && cp -R /navigator /srv/data/papyri.info/git/navigator/
 fi
 
+if [ ! -d "/srv/data/papyri.info/solr" ]; then
+  mkdir -p /srv/data/papyri.info && cp -R /navigator/pn-solr /srv/data/papyri.info/solr
+fi
+
 sed -i -e 's/localhost:8090/fuseki:8090/' /srv/data/papyri.info/git/navigator/pn-mapping/src/info/papyri/map.clj
 cd /srv/data/papyri.info/git/navigator/pn-mapping && lein run map-all
 
--- tomcat-solr/copy-solr-and-run.sh
+++ tomcat-solr/copy-solr-and-run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 if [ -e "/srv/data/papyri.info/git/navigator/pn-dispatcher/target/dispatch.war" ]; then
-  cp /srv/data/papyri.info/git/navigator/pn-dispatcher/target/dispatch.war /usr/local/tomcat/webapps/
-  catalina.sh run
+  cp -v /srv/data/papyri.info/git/navigator/pn-dispatcher/target/dispatch.war /usr/local/tomcat/webapps/dispatch.war
+  cp -v /root/solr.war /usr/local/tomcat/webapps/solr.war
+  # cp -v /root/tomcat-users.xml /usr/local/tomcat/conf/
+  JAVA_OPTS="-server -Xms1500m -Xmx2G -Xmn450m -XX:MaxPermSize=128m -verbose:gc -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+PrintTenuringDistribution -XX:+PrintHeapAtGC -XX:+UseConcMarkSweepGC -Dsolr.solr.home=/srv/data/papyri.info/solr" catalina.sh run
   exit 0
 else
   exit 1
