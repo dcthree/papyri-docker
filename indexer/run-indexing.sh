da7413aeeafd4ab9d035def0ef5eac7ceb0eb209
Ryan Baumann <ryan.baumann@gmail.com>
2017-07-12 15:41:48 -0400

    Only run indexing once

--- indexer/run-indexing.sh
+++ indexer/run-indexing.sh
@@ -8,6 +8,8 @@ if [ ! -d "/srv/data/papyri.info/git/navigator/epidoc-xslt" ]; then
   mkdir -p /srv/data/papyri.info/navigator/epidoc-xslt && cp -R /epidoc-xslt/example-p5-xslt /srv/data/papyri.info/git/navigator/epidoc-xslt
 fi
 
-sed -i -e 's/localhost:8090/fuseki:8090/' /srv/data/papyri.info/git/navigator/pn-indexer/src/info/papyri/indexer.clj
-sed -i -e 's/localhost:8083/solr:8080/' /srv/data/papyri.info/git/navigator/pn-indexer/src/info/papyri/indexer.clj
-cd /srv/data/papyri.info/git/navigator/pn-indexer && /root/wait-for-it.sh -t 9999 solr:8080 -- lein run
+if [ ! -e "/srv/data/papyri.info/indexing_done" ]; then
+  sed -i -e 's/localhost:8090/fuseki:8090/' /srv/data/papyri.info/git/navigator/pn-indexer/src/info/papyri/indexer.clj
+  sed -i -e 's/localhost:8083/solr:8080/' /srv/data/papyri.info/git/navigator/pn-indexer/src/info/papyri/indexer.clj
+  cd /srv/data/papyri.info/git/navigator/pn-indexer && /root/wait-for-it.sh -t 9999 solr:8080 -- lein run && touch /srv/data/papyri.info/indexing_done
+fi
