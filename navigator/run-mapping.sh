d9235ca9a14e5ce0a02049159eb20488f39363e6
Ryan Baumann <ryan.baumann@gmail.com>
2017-07-13 17:23:45 -0400

    Working pn-sync

--- navigator/run-mapping.sh
+++ navigator/run-mapping.sh
@@ -13,7 +13,16 @@ sed -i -e 's/litpap.info\/maven/dev.papyri.info\/maven/' /srv/data/papyri.info/g
 sed -i -e 's/localhost:8083/localhost:8080/' /srv/data/papyri.info/git/navigator/pn-sync/src/main/java/info/papyri/sync/Publisher.java
 sed -i -e 's/localhost:8090/fuseki:8090/' /srv/data/papyri.info/git/navigator/pn-sync/src/main/java/info/papyri/sync/GitWrapper.java
 sed -i -e 's/mysql:\/\/localhost/mysql:\/\/mysql/' /srv/data/papyri.info/git/navigator/pn-sync/src/main/java/info/papyri/sync/GitWrapper.java
+sed -i -e 's/canonical/origin/' /srv/data/papyri.info/git/navigator/pn-sync/src/main/java/info/papyri/sync/GitWrapper.java
+sed -i -e 's/github/origin/' /srv/data/papyri.info/git/navigator/pn-sync/src/main/java/info/papyri/sync/GitWrapper.java
+sed -i -e 's/master/v1.0-rc1/' /srv/data/papyri.info/git/navigator/pn-sync/src/main/java/info/papyri/sync/GitWrapper.java
+sed -i -e '/^.*git\.push.*$/d' /srv/data/papyri.info/git/navigator/pn-sync/src/main/java/info/papyri/sync/GitWrapper.java
 cd /srv/data/papyri.info/git/navigator/pn-sync && mvn clean compile war:war
+
+if [ ! -e "/srv/data/papyri.info/pn/docs" ]; then
+  mkdir -p /srv/data/papyri.info/pn
+  git clone https://github.com/papyri/site-docs.git /srv/data/papyri.info/pn/docs
+fi
 
 if [ ! -e "/srv/data/papyri.info/mapping_done" ]; then
   sed -i -e 's/localhost:8090/fuseki:8090/' /srv/data/papyri.info/git/navigator/pn-mapping/src/info/papyri/map.clj
