c488b3581828177b884f360558abe285bf7dfee0
Ryan Baumann <ryan.baumann@gmail.com>
2020-11-18 14:06:32 -0500

    Use stock SoSOL image base

--- sosol/migrate-and-copy.sh
+++ sosol/migrate-and-copy.sh
@@ -5,7 +5,7 @@ if true; then # [ ! -e "/srv/data/papyri.info/sosol/editor/editor.war" ]; then
   ./wait-for-it.sh -t 9999 mysql:3306
   rm -rf /srv/data/papyri.info/sosol/editor
   mkdir -p /srv/data/papyri.info/sosol
-  sed -i -e "s/NUMBERS_SERVER_DOMAIN = 'litpap.info'/NUMBERS_SERVER_DOMAIN = 'httpd'/" /root/sosol/lib/numbers_rdf.rb
+  sed -i -e "s/NUMBERS_SERVER_DOMAIN = 'papyri.info'/NUMBERS_SERVER_DOMAIN = 'httpd'/" /root/sosol/lib/numbers_rdf.rb
   cp -R /root/sosol /srv/data/papyri.info/sosol/editor
   cd /srv/data/papyri.info/sosol/editor && bundle exec rake db:migrate RAILS_ENV="production"
   cd /srv/data/papyri.info/sosol/editor && bundle exec warble war && touch editor.war.lock
