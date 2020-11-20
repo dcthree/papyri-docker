fa82cefb1b9a7c3c22237ec692230130ba432e56
Ryan Baumann <ryan.baumann@gmail.com>
2020-11-20 09:44:04 -0500

    Copy Solr config files into correct location

--- navigator/run-mapping.sh
+++ navigator/run-mapping.sh
@@ -4,8 +4,8 @@ if [ ! -d "/srv/data/papyri.info/git/navigator" ]; then
   mkdir -p /srv/data/papyri.info/git && cp -R /navigator /srv/data/papyri.info/git/navigator/
 fi
 
-if [ ! -d "/srv/data/papyri.info/solr" ]; then
-  mkdir -p /srv/data/papyri.info/solr && cp -R -v /navigator/pn-solr /solr
+if [ ! -e "/solr/solr.xml" ]; then
+  cp -R -v /navigator/pn-solr/. /solr/
 fi
 
 # pn-config
@@ -43,6 +43,8 @@ fi
 if [ ! -e "/srv/data/papyri.info/mapping_done" ]; then
   sed -i -e 's/localhost:8090/fuseki:8090/' /srv/data/papyri.info/git/navigator/pn-mapping/src/info/papyri/map.clj
   cd /srv/data/papyri.info/git/navigator/pn-mapping && lein run map-all && touch /srv/data/papyri.info/mapping_done
+  echo "mapping done"
 fi
 
+echo "sleep infinity"
 sleep infinity
