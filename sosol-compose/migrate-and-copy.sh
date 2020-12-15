8710123528ead6c38c6f0070c671a984dd26d808
Ryan Baumann <ryan.baumann@gmail.com>
2020-12-15 15:43:12 -0500

    Use compose-specific boot script with Git submodule Dockerfile for SoSOL

--- sosol-compose/migrate-and-copy.sh
+++ sosol-compose/migrate-and-copy.sh
@@ -7,14 +7,15 @@ until [ -e "${WAIT_LOCK}" ]; do
 done
 echo "${WAIT_LOCK} detected"
 
-if [ ! -e "/srv/data/papyri.info/sosol/editor/editor.war.lock" ]; then
-  echo "editor.war.lock missing, generating editor.war"
-  rm -fv /srv/data/papyri.info/sosol/editor/editor.war
-  ./wait-for-it.sh -t 9999 mysql:3306
+if true; then # [ ! -e "/srv/data/papyri.info/sosol/editor/editor.war.lock" ]; then
+  echo "generating editor.war"
+  rm -fv /srv/data/papyri.info/sosol/editor/editor.war /srv/data/papyri.info/sosol/editor/editor.war.lock
+  /opt/sosol-compose/wait-for-it.sh -t 9999 mysql:3306
   rm -rf /srv/data/papyri.info/sosol/editor
   mkdir -p /srv/data/papyri.info/sosol
   sed -i -e "s/NUMBERS_SERVER_DOMAIN = 'papyri.info'/NUMBERS_SERVER_DOMAIN = 'httpd'/" /root/sosol/lib/numbers_rdf.rb
   cp -R /root/sosol /srv/data/papyri.info/sosol/editor
+  cp -Rv /opt/sosol-compose/config/* /srv/data/papyri.info/sosol/editor/config
   cd /srv/data/papyri.info/sosol/editor && bundle exec rake db:migrate RAILS_ENV="production"
   cd /srv/data/papyri.info/sosol/editor && bundle exec warble war && touch editor.war.lock
 else
