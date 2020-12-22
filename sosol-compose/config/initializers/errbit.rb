c976243165db20863d7c81cd8ccda396730baa6b
Ryan Baumann <ryan.baumann@gmail.com>
2020-12-22 09:28:02 -0500

    Update errbit config

--- sosol-compose/config/initializers/errbit.rb
+++ sosol-compose/config/initializers/errbit.rb
@@ -2,9 +2,9 @@ Airbrake.configure do |config|
   config.host = 'https://errbit-dc3.herokuapp.com'
   config.project_id = 1 # required, but any positive integer works
   config.project_key = '2dacf6b2c62f2073bc14786504789294'
-  config.performance_stats = false
+  # config.performance_stats = false
 
   # Uncomment for Rails apps
   config.environment = Rails.env
-  config.ignore_environments = %w(development test)
+  # config.ignore_environments = %w(development test)
 end
