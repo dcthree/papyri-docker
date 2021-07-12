7a6d51e6bdf0fd82c3e29becd51e20d99990c6a6
Ryan Baumann <ryan.baumann@gmail.com>
2021-07-12 10:18:56 -0400

    Add lein run biblio && lein run load-lemmas to indexing

--- indexer/run-indexing.sh
+++ indexer/run-indexing.sh
@@ -29,7 +29,7 @@ if [ ! -e "${LOCK_FILE}" ]; then
   sed -i -e 's/localhost:8090/fuseki:8090/' /srv/data/papyri.info/git/navigator/pn-indexer/src/info/papyri/indexer.clj
   sed -i -e 's/localhost:8983/solr:8983/' /srv/data/papyri.info/git/navigator/pn-indexer/src/info/papyri/indexer.clj
   sed -i -e 's/Xmx1G/Xmx8G/' /srv/data/papyri.info/git/navigator/pn-indexer/project.clj
-  cd /srv/data/papyri.info/git/navigator/pn-indexer && /root/wait-for-it.sh -t 9999 solr:8983 -- sleep 30 && lein run && touch $LOCK_FILE
+  cd /srv/data/papyri.info/git/navigator/pn-indexer && /root/wait-for-it.sh -t 9999 solr:8983 -- sleep 30 && lein run biblio && lein run load-lemmas && lein run && touch $LOCK_FILE
   echo "indexing done"
 else
   echo "${LOCK_FILE} already exists, skipping indexing"
