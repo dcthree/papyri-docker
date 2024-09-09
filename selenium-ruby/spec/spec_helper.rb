da39994a67c06bc62936f2bd5ceb6ffd078ef45e
Isaiah Coleman <isaiah.coleman@duke.edu>
2024-09-09 15:49:47 -0400

    Locking to a stable version and changing spin up/tear down driver handling

--- selenium-ruby/spec/spec_helper.rb
+++ selenium-ruby/spec/spec_helper.rb
@@ -2,7 +2,12 @@
 require 'rails_helper'
 
 RSpec.configure do |config|
-  config.before(:each) do
+  config.before(:suite) do
     driven_by(:selenium_remote)
+  end
+
+  config.after(:suite) do
+    # Assuming we need to quit the driver, adjust as necessary
+    Capybara.current_session.driver.quit
   end
 end
