b2bb2938d2d098d7262fa2b16536d80de20ee426
Isaiah Coleman <isaiah.coleman@duke.edu>
2025-05-27 15:24:22 -0400

    Adds basic editor health check and updates submodule heads

--- selenium-ruby/spec/system/sosol/basic_health_spec.rb
+++ selenium-ruby/spec/system/sosol/basic_health_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Basic Health Check Tests", type: :system do
   it "should take a screenshot of homepage" do
-    visit '/'
-    save_screenshot('/opt/selenium/navigator_home.png')
+    visit '/editor/signin'
+    save_screenshot('/opt/selenium/sosol_home.png')
   end
   # go to google.com and test the page title
   it "should go to homepage and test page title" do
-    visit '/'
+    visit '/editor/signin'
     expect(page).to have_title('Papyri.info')
   end
 end
